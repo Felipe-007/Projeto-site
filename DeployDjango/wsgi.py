40fc63330c86c62ba524be168d3a30f0e882a00a
Felipe Lopes <felipe.presidencia@gmail.com>
2021-05-07 09:51:22 -0300

    Commit

--- DeployDjango/wsgi.py
+++ DeployDjango/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for carousel project.
+WSGI config for DeployDjango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carousel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DeployDjango.settings')
 
 application = get_wsgi_application()
